bca548e0b36fef16407af570fe0d65cbdbe7822c
fitriafadila <fitriafadhilah22@gmail.com>
2024-10-14 15:21:35 +0700

    add index and login page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/welcome', function () {
+    return view('welcome'); //ini merujuk ke halaman welcome.blade.php di folder resouces/view
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index'); //ini merujuk ke halaman welcome.blade.php resouces/view
+});
+
+Route::get('/login', function () {
+    return view('login'); //ini merujuk ke halaman welcome.blade.php resouces/view
 });
